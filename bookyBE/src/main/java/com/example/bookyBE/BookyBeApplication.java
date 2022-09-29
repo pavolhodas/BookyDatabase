df81a4a0b4337af2d6a2dd9748c3c4a44ff7686e
pavolhodas <71751188+pavolhodas@users.noreply.github.com>
2022-09-29 08:24:19 +0200

    Add files via upload

---
+++ bookyBE/src/main/java/com/example/bookyBE/BookyBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookyBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookyBeApplication.class, args);
+	}
+
+}
---
+++ bookyBE/src/test/java/com/example/bookyBE/BookyBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookyBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookyBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
